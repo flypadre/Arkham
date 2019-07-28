82a79f2f98da7498f319ec3df87d4068a65a8b89
Sean Morris <sean@oblate.me>
2019-07-28 14:01:56 -0400

    Added sprites for making stages of brokeness in bricks. Created multihit bricks.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    // configuration params
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
 
     // Cached Reference
     Level level;
+
+    // state variables
+    [SerializeField] int timesHit; // Todo only serialized for debug purposes
 
     private void Start()
     {
@@ -28,7 +34,34 @@ public class Block : MonoBehaviour
     {
         if (tag == "Breakable")
         {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
             DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array " + gameObject.name);
         }
     }
 
